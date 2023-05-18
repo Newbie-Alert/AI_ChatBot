f74ede27f686dabdfee7835c24418e4f6164bd34
NebieAlert <mjnn100801@naver.com>
2023-05-18 20:10:29 +0900

    첫 API활용

---
+++ api.js
@@ -0,0 +1,3 @@
+const API_KEY = {
+  key: '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'
+}
---
+++ chat.js
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from 'https://cdn.skypack.dev/openai';
+const chatInput = document.querySelector('.chat-input');
+const chatBtn = document.querySelector('.chat-button');
+const form = document.querySelector('.chat-form');
+const chatAi = document.querySelector('.chat-ai');
+const chatMe = document.querySelector('.chat-me');
+const container = document.querySelector('.container');
+
+const KEY = API_KEY.key;
+
+
+
+chatBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  container.innerHTML += `
+    <div class="chat-me">${chatInput.value}</div>
+  `
+
+
+  const configuration = new Configuration({
+    apiKey: KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: chatInput.value,
+    temperature: 0.8,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  }).then((result) => {
+    console.log(result.data);
+    container.innerHTML += `
+    <div class="chat-ai">${result.data.choices[0].text}</div>
+  `
+  })
+  chatInput.value = ``;
+})
+
